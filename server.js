48f0efe18b0d93d6afb941aaea2054dfdabb2051
homoereto <juarezfaisca4@gmail.com>
2025-09-29 18:26:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,56 +3,38 @@ const app = express();
 const http = require('http').createServer(app);
 const { Server } = require('socket.io');
 
-// Criando 3 instâncias de servidores de socket.io
+// Servidores separados por path
 const io1 = new Server(http, { path: '/server1' });
 const io2 = new Server(http, { path: '/server2' });
 const io3 = new Server(http, { path: '/server3' });
 
-// Servir arquivos estáticos
+// Serve arquivos estáticos
 app.use(express.static('public'));
 
-// Rota principal -> login
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/login.html');
-});
+// Rotas
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/login.html'));
+app.get('/chat1', (req, res) => res.sendFile(__dirname + '/public/chat1.html'));
+app.get('/chat2', (req, res) => res.sendFile(__dirname + '/public/chat2.html'));
+app.get('/chat3', (req, res) => res.sendFile(__dirname + '/public/chat3.html'));
 
-// Rotas dos chats
-app.get('/chat1', (req, res) => {
-  res.sendFile(__dirname + '/public/chat1.html');
-});
-app.get('/chat2', (req, res) => {
-  res.sendFile(__dirname + '/public/chat2.html');
-});
-app.get('/chat3', (req, res) => {
-  res.sendFile(__dirname + '/public/chat3.html');
-});
-
-// 🔹 Servidor 1
+// Sala 1
 io1.on('connection', socket => {
-  console.log('Usuário conectado ao Servidor 1');
-  socket.on('chat message', msg => {
-    io1.emit('chat message', msg);
-  });
+    console.log('Usuário conectado à Sala 1');
+    socket.on('chat message', msg => io1.emit('chat message', msg));
 });
 
-// 🔹 Servidor 2
+// Sala 2
 io2.on('connection', socket => {
-  console.log('Usuário conectado ao Servidor 2');
-  socket.on('chat message', msg => {
-    io2.emit('chat message', msg);
-  });
+    console.log('Usuário conectado à Sala 2');
+    socket.on('chat message', msg => io2.emit('chat message', msg));
 });
 
-// 🔹 Servidor 3
+// Sala 3
 io3.on('connection', socket => {
-  console.log('Usuário conectado ao Servidor 3');
-  socket.on('chat message', msg => {
-    io3.emit('chat message', msg);
-  });
+    console.log('Usuário conectado à Sala 3');
+    socket.on('chat message', msg => io3.emit('chat message', msg));
 });
 
 // Porta
 const PORT = process.env.PORT || 4000;
-http.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+http.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
